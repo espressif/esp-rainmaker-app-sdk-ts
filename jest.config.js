b5ce54c748062cb9f030bec15b9a965f57471c6b
Rushikesh Patange <rushikesh.patange@espressif.com>
2025-04-18 10:40:31 +0530

    CI: Integrate unit tests and notify test report workflow

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,24 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 
 export default {
-  projects: [
-    {
-      preset: "ts-jest",
-      testEnvironment: "node",
-      displayName: "@espressif/rainmaker-base-sdk",
-      testMatch: ["<rootDir>/__tests__/**/*.(spec|test).[jt]s?(x)"],
-      setupFiles: ["<rootDir>/jest.setup.ts"],
-    },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  displayName: "@espressif/rainmaker-base-sdk",
+  testMatch: ["<rootDir>/__tests__/**/*.(spec|test).[jt]s?(x)"],
+  setupFiles: ["<rootDir>/jest.setup.ts"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./",
+        outputName: "junit.xml",
+        suiteName: "jest tests",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+        ancestorSeparator: " > ",
+        usePathForSuiteName: true,
+      },
+    ],
   ],
 };
